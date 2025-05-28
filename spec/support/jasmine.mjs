5d7be8be26829b1c837dd42ba09a373fa06eef0e
Sean Durham <seandurham@asana.com>
2025-05-27 17:05:14 -0700

    Configures jasmine (specs failing)

---
+++ spec/support/jasmine.mjs
@@ -0,0 +1,16 @@
+const config = {
+  spec_dir: "spec",
+  spec_files: [
+    "**/*[sS]pec.?(m)js"
+  ],
+  helpers: [
+    "helpers/**/*.?(m)js"
+  ],
+  env: {
+    stopSpecOnExpectationFailure: false,
+    random: true,
+    forbidDuplicateNames: true
+  }
+};
+
+export default config;
